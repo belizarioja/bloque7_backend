25213145024b8a33d69cca13ede66b5a5eda38ca
Jesus Belizario <jesus@debian-belizarioj.pdvsa.com>
2021-09-07 15:28:24 -0400

    backup nueva base de datos y conectar bien a bloque 7

--- config/conexion2.js
+++ config/conexion2.js
@@ -1,25 +1,25 @@
 var mysql = require('mysql');
-/* var pool  = mysql.createConnection({
-  // host     : 'federcont.sytes.net',
-  host     : 'localhost',
-  user     : 'usuario',
-  password : '*Loemy4200',
-  database : 'bloque7_app',
-  multipleStatements: true
-}); */
 var pool  = mysql.createConnection({
+  host     : 'federcont.sytes.net',
+  // host     : 'localhost',
+  user     : 'root',
+  password : 'SEUZ4',
+  database : 'seuz0000177',
+  multipleStatements: true
+}); 
+/* var pool  = mysql.createConnection({
   host     : 'www.ejdevelop.com',
   user     : 'ejdevelo_bloque7',
   password : '*bloque7*',
   database : 'ejdevelo_bloque7',
   multipleStatements: true
-});
+}); */
 pool.connect(function (err) {
     if (err) {
       console.error(err);
       return;
     } else {
-      console.log('BD sistema is connected');
+      console.log('BD sistema SEUZ is connected');
     }
   });
 module.exports = pool;
