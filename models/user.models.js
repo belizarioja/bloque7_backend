09b4a392651dfa3c872fa7efc4de1fec063b643b
unknown <belizarioja@gmail.com>
2021-11-24 20:27:03 -0400

    agregar nuevo usuario vendedor cuado se loguea por primera vez

--- models/user.models.js
+++ models/user.models.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const conexion = require("../config/conexion")
+const conexion2 = require("../config/conexion2")
 const config = require("../config/general")
 const router = express.Router();
 
@@ -11,15 +12,64 @@ router.post(config.servidor + '/login', function (req, res) {
                 status: 400
             });
         } else {
-            const sql = "select a.id, a.nombre, a.usuario, a.idrol, a.status, b.rol " 
-            const from = "from usuarios a, roles b "
-            const from = "where a.usuario ='"+usuario+"' and a.clave = '"+clave+"' and a.idrol = b.id";
-            const resp = conexion.query(sql, function (err, rows) {
+            const sql1 = "select * from usuarios where usuario ='" + usuario + "'"
+            const sql2 = "select a.id, a.nombre, a.usuario, a.idrol, a.status, b.rol " 
+            const from2 = "from usuarios a, roles b "
+            const where2 = "where a.usuario ='" + usuario + "' and a.clave = '" + clave + "' and a.idrol = b.idROL";
+            const resp1 = conexion.query(sql1, function (err, rows) {
                 if(!err) {
-                    res.json(rows);
+                    if(rows.length > 0) {
+                        const resp2 = conexion.query(sql2 + from2 + where2, function (err, rows) {
+                            if(!err) {
+                                res.json(rows)
+                            } else {
+                                // console.log(err)
+                                res.json({ 
+                                    message: "Error al Acceso",
+                                    resp: err,
+                                    status: 500
+                                });
+                            }
+                        });
+                    }
+                    else {
+                        const sql3 = "select VENDV_NOMBRE, VENDV_IDSUCURSAL from tvendedores where VENDV_IDVENDEDOR ='" + usuario + "'";
+                        const resp3 = conexion2.query(sql3, function (err, rows) {
+                            // console.log("rows 2")
+                            if(rows.length > 0) {                               
+                                const idsucursal = rows[0].VENDV_IDSUCURSAL
+                                const nombre = rows[0].VENDV_NOMBRE
+                                const sql4 = "insert into usuarios (usuario, clave, nombre, idsucursal, idrol, status) "
+                                const values = " values ( ?, ?, ?, ?, ?, ?)"
+                                conexion.query(sql4 + values, [usuario, usuario, nombre, idsucursal, 3, 1], function (err, rows) {
+                                    if(!err) {
+                                        // console.log("Insertando ", rows.insertId)
+                                        const arreglo = []
+                                        const obj = {}
+                                        obj.id = rows.insertId
+                                        obj.idrol = 3
+                                        obj.idsucursal = idsucursal
+                                        obj.nombre = nombre
+                                        obj.rol = "Vendedor"
+                                        obj.usuario = usuario                                
+                                        arreglo.push(obj)
+                                        res.json(arreglo)
+                                    } else {
+                                        res.json({ 
+                                            message: "Error al Acceso",
+                                            resp: err,
+                                            status: 500
+                                        });
+                                    }
+                                });                               
+                            } else {
+                                res.json(rows)
+                            }                            
+                        });
+                    }                    
                 } else {
                     res.json({ 
-                        message: "Acceso NO válido",
+                        message: "Error al Acceso",
                         resp: err,
                         status: 500
                     });
